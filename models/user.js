256d451b3abb6f964e83f5025e4530bb73df2722
Pratik Shinde <pratikshinde1497@gmail.com>
2021-01-09 15:09:44 +0530

    add models: users, gUsers

---
+++ models/user.js
@@ -0,0 +1,19 @@
+module.exports = (sequelize, Sequelize) => {
+  const User = sequelize.define("user", {
+    name: {
+      type: Sequelize.STRING
+    },
+    email: {
+      type: Sequelize.STRING,
+      unique: true
+    },
+    password: {
+      type: Sequelize.STRING
+    },
+    role: {
+      type: Sequelize.ENUM('user', 'admin','publisher')
+    }
+  });
+
+  return User;
+};
